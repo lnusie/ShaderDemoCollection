309ccdceebfd3838c328b32cb5678a7983cbad5b
linyusi <linyusi@aobi.com>
2021-05-05 21:17:19 +0800

    feat：【平面反射】代码素材提交

---
+++ ShaderDemoCollection/Assets/XShader/Scene&Model/Reflect/WaterPlane.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WaterPlane : MonoBehaviour
+{
+    public RawImage m_RawImage;
+    public Camera m_SourceCam;
+    public Camera m_ReflectCam;
+    public int m_RTWidth = 1024;
+    private RenderTexture m_ReflectRT;
+    private Material m_Material;
+    private Matrix4x4 m_ReflectMatrix;
+
+    public int m_MaxWave;
+    private List<WaveInfo> m_Waves;
+
+    void Start()
+    {
+        GameObject go = new GameObject("ReflectCamera");
+        m_ReflectCam = go.AddComponent<Camera>();
+        m_ReflectCam.CopyFrom(m_SourceCam);
+        m_ReflectCam.clearFlags = CameraClearFlags.SolidColor;
+
+        RenderTexture rt = RenderTexture.GetTemporary(m_RTWidth, m_RTWidth * Screen.height / Screen.width);
+        m_ReflectCam.targetTexture = rt;
+        //m_RawImage.texture = rt;
+        m_ReflectRT = rt;
+        m_ReflectCam.transform.position = m_SourceCam.transform.position;
+        m_Material = GetComponent<MeshRenderer>().sharedMaterial;
+        m_Material.SetTexture("_MainTex", m_ReflectRT);
+
+        m_Waves = new List<WaveInfo>();
+        for (int i = 0; i < m_MaxWave; i++)
+        {
+            m_Waves.Add(new WaveInfo());
+        }
+    }
+
+    void Update()
+    {
+        m_ReflectCam.enabled = true;
+        Vector3 normal = transform.up;
+        Vector3 pos = transform.position;
+        float distance = -Vector3.Dot(normal, pos);
+        m_ReflectMatrix = CalcReflectMatrix(normal, distance);
+
+        m_ReflectCam.worldToCameraMatrix = m_SourceCam.worldToCameraMatrix * m_ReflectMatrix;
+
+        GL.invertCulling = true;
+        m_ReflectCam.Render();
+        GL.invertCulling = false;
+        m_ReflectCam.enabled = false;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                CreateWave(hitInfo.point);
+            }
+        }
+        UpdateWaves();
+    }
+
+    void CreateWave(Vector3 centerPos)
+    {
+        for (int i = 0; i < m_Waves.Count; i++)
+        {
+            var wave = m_Waves[i];
+            if (wave.m_State == 0)
+            {
+                wave.m_Vect = centerPos;
+                wave.m_State = 1;
+                wave.m_Range = 1;//Random.Range(0.2f, 2);
+                wave.m_WaveSpeed = 1;// Random.Range(1, 1.2f);
+                m_Material.SetFloat("_WaveRange" + i, wave.m_Range);
+                return;
+            }
+        }
+    }
+
+    void UpdateWaves()
+    {
+        for (int i = 0; i < m_Waves.Count; i++)
+        {
+            var wave = m_Waves[i];
+            if (wave.m_State == 1)
+            {
+                bool end = wave.Update();
+                if (end)
+                {
+                    wave.m_Vect.w = 999;
+                    m_Material.SetVector("_Wave" + i, wave.m_Vect);
+                    wave.m_State = 0;
+                    continue;
+                } 
+                m_Material.SetVector("_Wave" + i, wave.m_Vect);
+            }
+        }
+    }
+
+    Matrix4x4 CalcReflectMatrix(Vector3 normal, float d)
+    {
+        Matrix4x4 m = Matrix4x4.identity;
+        m.m00 = 1 - 2 * normal.x * normal.x;
+        m.m01 = -2 * normal.y * normal.x;
+        m.m02 = -2 * normal.z * normal.x;
+        m.m03 = -2 * d * normal.x;
+
+        m.m10 = -2 * normal.x * normal.y;
+        m.m11 = 1 - 2 * normal.y * normal.y;
+        m.m12 = -2 * normal.z * normal.y;
+        m.m13 = -2 * d * normal.y;
+
+        m.m20 = -2 * normal.x * normal.z;
+        m.m21 = -2 * normal.y * normal.z;
+        m.m22 = 1 - 2 * normal.z * normal.z;
+        m.m23 = -2 * d * normal.z;
+
+        m.m30 = 0;
+        m.m31 = 0;
+        m.m32 = 0;
+        m.m33 = 1;
+
+        return m;
+    }
+
+    void OnDestroy()
+    {
+        if (m_ReflectRT != null)
+        {
+            RenderTexture.ReleaseTemporary(m_ReflectRT);
+            m_ReflectRT = null;
+        }
+    }
+
+    public class WaveInfo
+    {
+        public int m_State;
+        public Vector4 m_Vect;
+        public float m_WaveSpeed;
+        public float m_Range;
+
+        public bool Update()
+        {
+            m_Vect.w += m_WaveSpeed * Time.deltaTime;
+            return m_Vect.w > m_Range;
+        }
+    }
+       
+}
---
+++ ShaderDemoCollection/Assets/XShader/Scene&Model/Reflect/WaterPlane.shader
@@ -0,0 +1,97 @@
+﻿Shader "X_Shader/Scene/WaterPlane"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "black" {}
+        _Wave0("Wave0", Vector) = (0,0,0,0)
+        _WaveRange0("Wave0", float) = 1
+        _RippleTex("Ripple Tex", 2D) = "black" {}
+    }
+    SubShader
+    {
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float4 screenPos : TEXCOORD1;
+                float4 worldPos : TEXCOORD2;
+
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.screenPos = ComputeScreenPos(o.vertex);
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+            sampler2D _RippleTex;
+            
+            float4 _Wave0;
+            float _WaveRange0;
+            float4 _RippleTex_ST;
+            float _RippleScale;
+
+            float GetWaveValue(float x, float y)
+            {
+                return ((sin(x * 20 + (3.14/2))+1) / max(1, abs(x *8))) * 0.5 * y;
+            } 
+            
+            float GetWavePower(v2f i, float4 wave, float range, inout float3 offset)
+            {
+                float4 worldPos = i.worldPos;
+                float3 dir = worldPos.xyz - wave.xyz;
+                float dis = length(dir);
+                float powerFactor = (dis / range);
+                powerFactor = max(0, 1 - powerFactor);
+
+                float waveRange = 0.3;
+                float value = (dis - wave.w)/waveRange;
+                value = GetWaveValue(value, powerFactor);
+                offset = normalize(dir);
+                return value;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float2 uv = i.uv;
+                float4 worldPos = i.worldPos;
+                float3 offset0;
+                float power = GetWavePower(i, _Wave0, _WaveRange0, offset0);
+                float4 screenPos = i.screenPos;
+                float time = frac(_Time.y * 0.8);
+                float uv_move = floor(_Time.y * 0.5) * 0; 
+                float4 ripple = tex2D(_RippleTex, TRANSFORM_TEX(uv, _RippleTex) + uv_move);
+                time = frac((_Time.y * 0.5 + ripple.a));
+                float ripple_value = (time) - 1 + ripple.x;
+                ripple_value = clamp(ripple_value * 9.0, 0.0, 5.0);
+                float ripplePower = sin(ripple_value * 3.14) / (ripple_value + 1);
+                ripplePower *= (1 - time);
+                ripple.yz = ripple.yz * 2 - 1;
+                screenPos.xy += ripple.yz  * ripplePower;
+                fixed4 col = tex2Dproj(_MainTex, screenPos);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
